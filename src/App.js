58ccdb8e0e380fb90faa3bec4aed6894c184ea8b
Karthi3002 <164326735+Karthi3002@users.noreply.github.com>
2025-05-29 13:51:35 +0530

    c2

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import './styles/global.css';
 import About from './components/AboutUs';
 import Features from './components/Features';
 import ComingSoon from './components/ComingSoon';
+import Testimonial from './components/Testimonial';
+import Stats from './components/Stats';
+import Services from './components/Services';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 function App() {
   useEffect(() => {
     AOS.init({
-      duration: 1000, // animation duration
-      once: true,     // whether animation should happen only once - while scrolling down
+    once: true, // Animate only once
+    offset: 120,
+    duration: 800,
+    easing: 'ease-in-out',    // whether animation should happen only once - while scrolling down
     });
   }, []);
 
@@ -20,6 +25,9 @@ function App() {
       <About />
       <Features />
       <ComingSoon />
+      <Testimonial />
+      <Stats />
+      <Services />
     </div>
   );
 }
